0d8b7c28005a1f9aacc0f275456ae0dccc4436e5
lokesheltropy <40492338+lokesheltropy@users.noreply.github.com>
2024-03-08 16:18:59 +0530

    up video link testing in flutter app

---
+++ browser_app/lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+// Import for Android features.
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+// Import for iOS features.
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(useMaterial3: true),
+    home: WebViewApp(),
+  ));
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    controller = WebViewController.fromPlatformCreationParams(params);
+// ···
+    if (controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+
+    controller.loadRequest(
+      Uri.parse('https://google.com'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UP Video'),
+      ),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}
+// // Define a custom Form widget.
+// class MyCustomForm extends StatefulWidget {
+//   const MyCustomForm({super.key});
+
+//   @override
+//   State<MyCustomForm> createState() => _MyCustomFormState();
+// }
+
+// // Define a corresponding State class.
+// // This class holds data related to the Form.
+// class _MyCustomFormState extends State<MyCustomForm> {
+//   // Create a text controller and use it to retrieve the current value
+//   // of the TextField.
+//   final myController = TextEditingController();
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     // Start listening to changes.
+//     myController.addListener(_printLatestValue);
+//   }
+
+//   @override
+//   void dispose() {
+//     // Clean up the controller when the widget is removed from the widget tree.
+//     // This also removes the _printLatestValue listener.
+//     myController.dispose();
+//     super.dispose();
+//   }
+
+//   void _printLatestValue() {
+//     final text = myController.text;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Video Testing'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16),
+//         child: Column(
+//           children: [
+//             TextField(
+//               controller: myController,
+//             ),
+//             TextButton(
+//                 onPressed: () {
+//                   final text = myController.text;
+//                   print('Url text field: $text (${text.characters.length})');
+//                 },
+//                 child: Text('Go'))
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
